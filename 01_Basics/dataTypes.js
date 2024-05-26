e08527e9b8758e888b649d81105745766b92927a
Shreyash Rajanand Dahiwale <61499836+ShreyashDahiwale@users.noreply.github.com>
2024-05-26 09:58:06 +0000

    DataTypes

---
+++ 01_Basics/dataTypes.js
@@ -0,0 +1,23 @@
+// JavaScript evolved everytime througout the period
+// The JavaScript commitee don't want that the Old code get disturb
+// And they want everyone should get same standard of JavaScript
+// So that's why we use "use strict"
+
+"use strict"; // Treat all JS Code as newer version 
+
+/*
+number
+bigInt
+string
+boolean
+null => standalone value
+undefined
+    symbol => unique
+*/
+
+
+// Object
+
+console.log(typeof"Shreyash");
+console.log(typeof null);  // Object
+console.log(typeof undefined);  // undefined
