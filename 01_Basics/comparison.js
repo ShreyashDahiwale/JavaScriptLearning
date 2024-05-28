c4112ccfa29fa02d64f5706af0c7ba28502759a9
Shreyash Rajanand Dahiwale <61499836+ShreyashDahiwale@users.noreply.github.com>
2024-05-28 16:19:51 +0000

    comparison

---
+++ 01_Basics/comparison.js
@@ -0,0 +1,12 @@
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);      // null is converted to 0
+console.log(null == 0);
+console.log(null >= 0); // False
+
+console.log(undefined == 0);
+
+// Strict check: ===
+
+console.log("2" === 2);  // also check datatypes
