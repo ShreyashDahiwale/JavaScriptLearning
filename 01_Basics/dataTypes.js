3dbb837f59ecc8d8810fa976e4111edffefbdbfa
Shreyash Rajanand Dahiwale <61499836+ShreyashDahiwale@users.noreply.github.com>
2024-05-28 16:38:02 +0000

    more discussion on DataTypes

--- 01_Basics/dataTypes.js
+++ 01_Basics/dataTypes.js
@@ -21,3 +21,36 @@ undefined
 console.log(typeof"Shreyash");
 console.log(typeof null);  // Object
 console.log(typeof undefined);  // undefined
+
+
+// ================================================================
+
+// Primitive
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// Reference (Non Primitive)
+// Array, Objects, Functions
+
+// Symbol 
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+console.log(id === anotherId);
+
+//  -------------------------------------------------------------------------------
+
+const heros = ["Shaktiman", "Naagraj", "Doga"];
+
+const myObj = {
+    firstName: "Shreyash",
+    age: 24
+}
+
+const myfunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof heros);       // type of Array is also an Object
+console.log(typeof myfunction);    // Object Function
+console.log(typeof myObj);
+myfunction();
