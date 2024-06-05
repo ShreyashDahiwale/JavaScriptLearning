ea41807ca0730d3bdc2afbad0c146af8ed08f76e
Shreyash Rajanand Dahiwale <61499836+ShreyashDahiwale@users.noreply.github.com>
2024-06-05 16:31:35 +0000

    Function Part - II

---
+++ 03_BasicsPlus/functionPartTwo.js
@@ -0,0 +1,32 @@
+function calculateCartPrice(...num1) {
+    return num1;
+}
+
+console.log(calculateCartPrice(200, 400, 500, 2000));
+
+function calculateCartAmount(val1, val2, ...num1) {
+    return num1;
+}
+console.log(calculateCartAmount(200, 400, 500, 2000));
+
+const user = {
+    username: "Shreyash",
+    ID: 199
+}
+
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject?.username} and ID is ${anyobject?.ID}`);
+}
+
+handleObject(user)
+
+///////////////// Scopes /////////////////////
+
+if (true) {
+    let a = 10
+    const b = 20
+    var c = 30
+}
+console.log(a);
+console.log(b);
+console.log(c);
