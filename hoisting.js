a8242408924f4cbf68ef457fcfa8431312db6d48
Shreyash Rajanand Dahiwale <61499836+ShreyashDahiwale@users.noreply.github.com>
2025-07-08 14:54:32 +0000

    Hoisting learning

---
+++ hoisting.js
@@ -0,0 +1,14 @@
+console.log(b);  // undefined
+console.log(a);  // throws an error Cannot access 'a' before initialization
+// Here, "a" was hoisted but memory is allocated in different space 
+// and it is not initailized. This is called Temporal Dead Zone
+// Temporal Dead Zone ends when the value is assigned to variable "a" by thecode
+// And whenever we access the that variable in the Temporal Dead Zone then we get Reference Error
+
+console.log(x); // It gives x is not defined
+
+let a = 10;     // this is in separate memory space
+var b =100;     // this variable is attached to the global execution context
+
+
+// console.log(window.b);
